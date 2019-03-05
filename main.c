762ce2e20ef8b1c08dd4b02b21ea46fc9853c570
lolkusus <lolkusus@users.noreply.github.com>
2019-03-05 23:01:22 +0100

    Zad_12

--- main.c
+++ main.c
@@ -20,13 +20,24 @@ int main()
 	
 	IO1DIR = LED0_bm | LED1_bm | LED2_bm | LED3_bm;
 	
-	IO1SET = LED0_bm;
-	IO1SET = LED1_bm;
-	IO1SET = LED2_bm;
-	IO1SET = LED3_bm;
-	
 	while(1)
 	{
+		
+		IO1SET = LED0_bm;
+		Delay(250);
+		IO1CLR = LED0_bm;
+		
+		IO1SET = LED1_bm;
+		Delay(250);
+		IO1CLR = LED1_bm;
+
+		IO1SET = LED2_bm;
+		Delay(250);
+		IO1CLR = LED2_bm;
+
+		IO1SET = LED3_bm;
+		Delay(250);
+		IO1CLR = LED3_bm;
 		
 	}
 	
