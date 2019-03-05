5e9bb712bb171246d51bd0078563b97ef1a2ecdd
lolkusus <lolkusus@users.noreply.github.com>
2019-03-05 22:56:57 +0100

    Zad_11

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <LPC21xx.H>
 
 #define LED0_bm 0x10000
+#define LED1_bm 0x20000
+#define LED2_bm 0x40000
 #define LED3_bm 0x80000
 
 void Delay(unsigned int uiDelay) 
@@ -16,15 +18,15 @@ void Delay(unsigned int uiDelay)
 int main()
 {
 	
-	IO1DIR = LED3_bm;
+	IO1DIR = LED0_bm | LED1_bm | LED2_bm | LED3_bm;
+	
+	IO1SET = LED0_bm;
+	IO1SET = LED1_bm;
+	IO1SET = LED2_bm;
+	IO1SET = LED3_bm;
 	
 	while(1)
 	{
-		
-		Delay(50);
-		IO1SET = LED3_bm;
-		Delay(50);
-		IO1CLR = LED3_bm;
 		
 	}
 	
