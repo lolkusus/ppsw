a93a78d0711db30688b18ac7c61f1c0b3c83b1de
lolkusus <lolkusus@users.noreply.github.com>
2019-03-18 18:33:30 +0100

    Zad_6_2

--- main.c
+++ main.c
@@ -12,21 +12,37 @@ void Delay(unsigned int uiDelay)
 
 int main()
 {
-	enum LedState{LEFT, RIGHT};
-	enum LedState eLedState = LEFT;
+	enum LedState{STATE0, STATE1, STATE2, STATE3, STATE4, STATE5};
+	enum LedState eLedState = STATE0;
 	LedInit();
 	KeyboardInit();
 	
 	while(1)
 	{
 		switch(eLedState){
-			case LEFT:
-				LedStepRight();
-				eLedState = RIGHT;
-				break;
-			case RIGHT:
+			case STATE0:
 				LedStepLeft();
-				eLedState = LEFT;
+				eLedState = STATE1;
+				break;
+			case STATE1:
+				LedStepLeft();
+				eLedState = STATE2;
+				break;
+			case STATE2:
+				LedStepLeft();
+				eLedState = STATE3;
+				break;
+			case STATE3:
+				LedStepRight();
+				eLedState = STATE4;
+				break;
+			case STATE4:
+				LedStepRight();
+				eLedState = STATE5;
+				break;
+			case STATE5:
+				LedStepRight();
+				eLedState = STATE0;
 				break;
 		}
 		Delay(250);
