40e613e162d964b96b3e1e481a36447a1a7d6373
lolkusus <lolkusus@gmail.com>
2019-05-29 08:48:32 +0200

    Zad_13_final_mofos

--- main.c
+++ main.c
@@ -11,22 +11,22 @@
 //ADControl
 #define ADPinSel (1<<22)
 #define ADCRSel (1<<0)
-#define ADCClks (7<<17)
 #define ADCEnable (1<<21)
 #define ADCStart (1<<24)
+#define ADCBURST (1<<16)
 //ADData
 #define ADCDone (1<<31)
 #define ADR_bm 0xFFC0
 
-unsigned char servoPos;
+long servoPos;
 
 void ADInit()
 {
 	PINSEL1 = ADPinSel; 
-	ADCR = ADCRSel | ADCClks | ADCEnable;
+	ADCR = ADCRSel |  ADCEnable;
 }
 
-unsigned int ADRead()
+long ADRead()
 {
 	ADCR = ADCR | ADCStart;
 	while((ADDR & ADCDone) == 0) {}
@@ -34,14 +34,15 @@ unsigned int ADRead()
 }
 
 int main (){
-	ServoInit(50);
+	ServoInit(150);
 	ADInit();
 	ServoCallib();
 	servoPos = 0;
 	
 	while(1)
 	{
-		servoPos = (ADRead() / 21);
+		servoPos = ADRead();
+		servoPos = servoPos / 21;
 		ServoGoTo(servoPos);
 	}
  
